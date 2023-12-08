05d9962e81b700a0ca99fcb3b7fcab36f2fceeac
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2023-12-08 15:27:07 +0100

    ver 0.18 - Added blocks to Hobbies section.

--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -10,10 +10,24 @@ export class HobbiesComponent implements OnInit {
   hobbies = [
     {
       hobbyPicture: "assets/icons/Development.svg",
-      hobbyName: "Programming",
+      hobbyName: "Programming . . . Computers",
       hobbyDescription: "Ad consectetur deserunt dicta dignissimos dolore dolorem doloremque " +
         "error est eveniet fugit id, ipsum laboriosam maxime natus non optio, possimus provident.",
-      hobbyLink: "https://github.com/l-Aleksey-Nikolaev-l"
+      hobbyLink: "https://github.com/l-Aleksey-Nikolaev-l?tab=repositories"
+    },
+    {
+      hobbyPicture: "assets/icons/Photocamera.svg",
+      hobbyName: "Photography . . . Videography",
+      hobbyDescription: "Ad consectetur deserunt dicta dignissimos dolore dolorem doloremque " +
+        "error est eveniet fugit id, ipsum laboriosam maxime natus non optio, possimus provident.",
+      hobbyLink: "https://www.instagram.com/mister.nikolson/"
+    },
+    {
+      hobbyPicture: "assets/icons/Maps.svg",
+      hobbyName: "Science . . . Travels",
+      hobbyDescription: "Ad consectetur deserunt dicta dignissimos dolore dolorem doloremque " +
+        "error est eveniet fugit id, ipsum laboriosam maxime natus non optio, possimus provident.",
+      hobbyLink: "https://www.researchgate.net/profile/Aleksei-Nikolaev"
     }
   ]
 
