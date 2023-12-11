c84b76d93e07447fb80bca4a5fe40828de65e5aa
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2023-12-11 11:35:04 +0100

    ver 0.19 - Finished with Hobbies section.

--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -11,22 +11,24 @@ export class HobbiesComponent implements OnInit {
     {
       hobbyPicture: "assets/icons/Development.svg",
       hobbyName: "Programming . . . Computers",
-      hobbyDescription: "Ad consectetur deserunt dicta dignissimos dolore dolorem doloremque " +
-        "error est eveniet fugit id, ipsum laboriosam maxime natus non optio, possimus provident.",
+      hobbyDescription: "I'm keen on various programming languages and use them in microelectronics and OS's. " +
+        "I'm interested in modern computers as in vintage computers and electronics chips. " +
+        "I'm familiar with Arduino, Atmel, Pic and programmed them",
       hobbyLink: "https://github.com/l-Aleksey-Nikolaev-l?tab=repositories"
     },
     {
       hobbyPicture: "assets/icons/Photocamera.svg",
       hobbyName: "Photography . . . Videography",
-      hobbyDescription: "Ad consectetur deserunt dicta dignissimos dolore dolorem doloremque " +
-        "error est eveniet fugit id, ipsum laboriosam maxime natus non optio, possimus provident.",
+      hobbyDescription: "I like to take photos on digital and film cameras and then develop them. " +
+        "Usually I work in Photoshop & Capture One. " +
+        "From time to time I like to make videos and edit them in DaVinci Resolve",
       hobbyLink: "https://www.instagram.com/mister.nikolson/"
     },
     {
       hobbyPicture: "assets/icons/Maps.svg",
       hobbyName: "Science . . . Travels",
-      hobbyDescription: "Ad consectetur deserunt dicta dignissimos dolore dolorem doloremque " +
-        "error est eveniet fugit id, ipsum laboriosam maxime natus non optio, possimus provident.",
+      hobbyDescription: "I really like travels. I visited many countries by car because I have a driver's license I like adventures. " +
+        "Also I like science and from time to time I write scientific articles ",
       hobbyLink: "https://www.researchgate.net/profile/Aleksei-Nikolaev"
     }
   ]
