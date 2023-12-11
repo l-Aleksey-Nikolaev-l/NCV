a2a38ef37e414727b2bc383c48ff0c1538501680
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2023-12-11 17:19:17 +0100

    ver 0.20 - Added the information to Contacts section.

--- src/app/top-area/top-area.component.ts
+++ src/app/top-area/top-area.component.ts
@@ -6,18 +6,3 @@ import { Component } from '@angular/core';
   styleUrls: ['./top-area.component.css']
 })
 export class TopAreaComponent {}
-
-
-document.addEventListener("mousemove", e => {
-  let raw_x = (e.clientX - (window.innerWidth / 2)) * -0.005;
-  let raw_y = (e.clientY - (window.innerHeight / 2)) * -0.01;
-
-  Object.assign(document.documentElement, {
-    style: `
-    --screenWidth: ${window.innerWidth};
-    --screenHeight: ${window.innerHeight};
-    --mouse_x: ${raw_x}deg;
-    --mouse_y: ${raw_y}deg;
-    `
-  })
-})
