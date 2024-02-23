2100bd4d466e8d81a8761a3bf43679d00c45ec4e
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2024-02-23 15:37:48 +0100

    ver 0.40 Added a new project CUE Sheet Audio. Added Cue_Sheet.gif for CUE Sheet Audio project.

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -11,13 +11,22 @@ export class ProjectsComponent {
   projectsData = [
     {
       projectName: "Currency Chart",
-      picture: "assets/img/Currency_Chart.PNG",
+      picture: "assets/img/Currency_Chart.gif",
       description: "A web-app with a chart for comparing currencies. " +
         "The app contains a user management panel. A client was built " +
         "using Angular, CSS, HTML and Capture One. Server side: NodeJS and " +
         "Firebase database. Login to the app: Admin / Password: 12345",
       projectLink: "https://currency-chart.vercel.app/Login",
       sourceLink: "https://github.com/l-Aleksey-Nikolaev-l/CurrencyChart"
+    },
+    {
+      projectName: "CUE Sheet Audio",
+      picture: "assets/img/Cue_Sheet.gif",
+      description: "This extension was created to make it easier to create and edit " +
+        "CUE Sheets of Audio files in VS Code. It supports syntax of CUE Sheet language. " +
+        "Built using JSONs and tmLanguage in VSCode",
+      projectLink: "https://marketplace.visualstudio.com/items?itemName=QuantumPannonia.cue-sheet-audio",
+      sourceLink: "https://github.com/l-Aleksey-Nikolaev-l/CUE-Sheet-Audio"
     },
     {
       projectName: "PhotoTransfer",
