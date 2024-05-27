f45c369038669ce4f80926402001d5d8e59c44a0
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2024-05-27 11:55:49 +0200

    ver 0.49 Fixed mistakes in hobbies.component.ts

--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -13,22 +13,22 @@ export class HobbiesComponent implements OnInit {
       hobbyName: "Programming . . . Computers",
       hobbyDescription: "I'm keen on various programming languages and use them in microelectronics and OS's. " +
         "I'm interested in modern computers as in vintage computers and electronics chips. " +
-        "I'm familiar with Arduino, Atmel, Pic and programmed them",
+        "I'm familiar with Arduino, Atmel, PIC and programmed them",
       hobbyLink: "https://github.com/l-Aleksey-Nikolaev-l?tab=repositories"
     },
     {
       hobbyPicture: "assets/icons/Photocamera.svg",
       hobbyName: "Photography . . . Videography",
-      hobbyDescription: "I like to take photos on digital and film cameras and then develop them. " +
-        "Usually I work in Photoshop & Capture One. " +
+      hobbyDescription: "I like taking photos on digital and film cameras and then develop them. " +
+        "Usually I work in Capture One and Photoshop. " +
         "From time to time I like to make videos and edit them in DaVinci Resolve",
       hobbyLink: "https://www.instagram.com/mister.nikolson/"
     },
     {
       hobbyPicture: "assets/icons/Maps.svg",
       hobbyName: "Science . . . Traveling",
-      hobbyDescription: "I really like travels. I visited many countries by car because I have a driver's license I like adventures. " +
-        "Also I like science and from time to time I write scientific articles ",
+      hobbyDescription: "I'm fond of traveling. I've visited many countries by car, it's adventurous. " +
+        "Also, I like science and from time to time I write scientific articles",
       hobbyLink: "https://www.researchgate.net/profile/Aleksei-Nikolaev"
     }
   ]
