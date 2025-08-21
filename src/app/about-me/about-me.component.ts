32c4be2c35cc56d29cb3df4d53d4caab9f088185
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2025-08-21 13:26:54 +0200

    refactor: change component about-me

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent {
-
+	captionAbout: string = 'Hi, My name is Aleksey Nikolaev';
+	titleAbout: string = 'I\'m a software engineer with 8 years’ experience ' +
+		'in programming industry. I\'m looking for a web developer position ' +
+		'in a modern company with possibilities of self-development and ' +
+		'dive deeper to this field. I have a modern frontend development ' +
+		'skills stack and solid knowledge of common design patterns. ' +
+		'Moreover, my ability to self-education and self-organization ' +
+		'allow me to work just as well on team projects as on my own.';
 }
